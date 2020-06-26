ddf423fb816f27d78e8475202f77ebeb1812bda5
Evgeny Samitin <samitin.e@gmail.com>
2020-06-26 21:45:23 +1100

    закончил работу

--- app/src/main/java/ru/samitin/mvp/Fragment1.kt
+++ app/src/main/java/ru/samitin/mvp/Fragment1.kt
@@ -11,18 +11,41 @@ import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment1.*
 
 class Fragment1: Fragment() {
+
+    val BUNDLE_COUNTENT="bundle_countent"
+
+        /* fun myFragment1( content:String):Fragment1{
+         val fragment=Fragment1()
+         val argument=Bundle()
+         argument.putString(BUNDLE_COUNTENT,content)
+         fragment.arguments=argument
+         return fragment
+     }*/
+    interface OnClickButton{
+            fun onClickAdd(name:String){
+
+            }
+            fun onClickClear(){
+
+            }
+        }
     
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView=inflater.inflate(R.layout.fragment1,container,false)
-        
+
         val btnAdd=rootView.findViewById<Button>(R.id.addBtn)
         val btnClear=rootView.findViewById<Button>(R.id.clearBtn)
         val et=rootView.findViewById<EditText>(R.id.etText)
+        et.setText("")
+        val click=activity as OnClickButton
         btnAdd.setOnClickListener {
-            Toast.makeText(activity,"btnAdd",Toast.LENGTH_SHORT).show()
+            Toast.makeText(activity,et.text.toString(),Toast.LENGTH_SHORT).show()
+            click.onClickAdd(et.text.toString())
+            et.setText("")
         }
-        clearBtn.setOnClickListener {
+        btnClear.setOnClickListener {
             Toast.makeText(activity,"btnClear",Toast.LENGTH_SHORT).show()
+            click.onClickClear()
         }
         return rootView
     }
--- app/src/main/java/ru/samitin/mvp/Fragment2.kt
+++ app/src/main/java/ru/samitin/mvp/Fragment2.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment2.*
 
 class Fragment2: Fragment() {
+    fun setText(text:String){
+        tv?.text=text
+    }
+    var tv:TextView?=null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView=inflater.inflate(R.layout.fragment2,container,false)
+
+        tv=rootView.findViewById<TextView>(R.id.textView)
         return rootView
     }
 }
